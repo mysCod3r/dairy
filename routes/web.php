3e06ab96716ee2712e78f2ce3c2a25457bbf4e5b
Muhammed Yasin Senocak <m.yasinsenocak@gmail.com>
2022-08-03 07:09:13 +0300

    Controller dosyalari düzenlendi

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,30 @@
 <?php
 
-
+use App\Http\Controllers\Frontend\DashboardController;
+use App\Http\Controllers\Frontend\FeedController;
+use App\Http\Controllers\Frontend\HakkimizdaController;
+use App\Http\Controllers\Frontend\LoginController;
+use App\Http\Controllers\Frontend\SignUpController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ControllerSignUp;
-use App\Http\Controllers\ControllerLogin;
-use App\Http\Controllers\ControllerHakkimizda;
-use App\Http\Controllers\ControllerFeed;
+
 
 
 Route::view('/', "pages.main")->name("anasayfa");
 
 
-Route::controller(ControllerHakkimizda::class)->group(function (){
-    Route::get("/about", [ControllerHakkimizda::class, 'index'])->name('goAbout');
+Route::controller(HakkimizdaController::class)->group(function (){
+    Route::get("/about", [HakkimizdaController::class, 'index'])->name('goAbout');
 });
 
-Route::controller(ControllerFeed::class)->group(function (){
-    Route::get("/feeds", [ControllerFeed::class, 'showAll'])->name('goFeeds');
+Route::controller(FeedController::class)->group(function (){
+    Route::get("/feeds", [FeedController::class, 'index'])->name('goFeeds');
 });
 
-Route::controller(ControllerSignUp::class)->group(function () {
-    Route::get('signup', [ControllerSignUp::class, 'index'])->name('goSignUp');
-    Route::post('signup/deneme', [ControllerSignUp::class, 'store'])->name('deneme');
-});
 
-Route::controller(ControllerLogin::class)->group(function () {
-    Route::get('login', [ControllerLogin::class, 'index'])->name('goLogin');
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::get('/token/create', [DashboardController::class, 'showTokenForm'])->name('token.showForm');
+    Route::post('/token/create', [DashboardController::class, 'createToken'])->name('token.create');
+    Route::post('/token/delete/{token}', [DashboardController::class, 'deleteToken'])->name('token.delete');
 });
+require __DIR__.'/auth.php';
